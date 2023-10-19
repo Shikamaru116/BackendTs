b0251e3de46ca7db2e60a629bed17f414872e55b
Shikamaru116 <gtapark116@gmail.com>
2023-10-19 00:03:12 -0600

    actualizacion de la tarea

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
 const express = require("express")
+import router from "./src/routes";
+
 const app = express();
 
-const example = function(){
-    console.log("hola que hace XD")
+const port = 9000;
+
+const initialize = () => {
+    console.log(`Hola que hace XD, el server se esta corriendo en el puerto ${port}`);
 }
 
-app.listen(9000, example)
+
+router(app);
+
+
+
+app.listen(port, initialize);
